1154b91f083116de06b77059634dbd812d0ec3c3
DruidYun <DruidYun@outlook.com>
2025-08-07 15:20:45 +0800

    添加编辑器工具模块

---
+++ Source/ZhaMUtilsEditor/Private/ZhaMUtilsEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "ZhaMUtilsEditor.h"
+
+#define LOCTEXT_NAMESPACE "FZhaMUtilsEditorModule"
+
+void FZhaMUtilsEditorModule::StartupModule()
+{
+    
+}
+
+void FZhaMUtilsEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FZhaMUtilsEditorModule, ZhaMUtilsEditor)
---
+++ Source/ZhaMUtilsEditor/Public/ZhaMUtilsEditor.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FZhaMUtilsEditorModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+};
