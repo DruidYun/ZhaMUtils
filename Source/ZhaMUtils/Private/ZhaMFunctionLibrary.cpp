0f4154fa3eef4789482d701ad1a47e0ebc3bc656
DruidYun <DruidYun@outlook.com>
2025-08-07 10:02:00 +0800

    Initial commit of ZhaMUtils plugin

---
+++ Source/ZhaMUtils/Private/ZhaMFunctionLibrary.cpp
@@ -0,0 +1,15 @@
+﻿#include "ZhaMFunctionLibrary.h"
+#include "Engine/Engine.h"
+
+void UZhaMFunctionLibrary::PrintLog(const FString& Message, float Duration)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, Duration, FColor::Green, Message);
+	}
+}
+
+int32 UZhaMFunctionLibrary::AddInts(int32 A, int32 B)
+{
+	return A + B;
+}
---
+++ Source/ZhaMUtils/Public/ZhaMFunctionLibrary.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ZhaMFunctionLibrary.generated.h"
+
+UCLASS()
+class ZHAMUTILS_API UZhaMFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	/** 打印一条绿色调试信息 */
+	UFUNCTION(BlueprintCallable, Category="ZhaMUtils|Debug")
+	static void PrintLog(const FString& Message, float Duration = 2.0f);
+
+	/** 计算两个数的和 */
+	UFUNCTION(BlueprintPure, Category="ZhaMUtils|Math")
+	static int32 AddInts(int32 A, int32 B);
+};
