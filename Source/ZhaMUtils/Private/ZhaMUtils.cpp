57c1546b87d261ab93034a5013b97c1448af5bbb
DruidYun <DruidYun@outlook.com>
2025-08-07 10:36:28 +0800

    模块

--- Source/ZhaMUtils/Private/ZhaMUtils.cpp
+++ Source/ZhaMUtils/Private/ZhaMUtils.cpp
@@ -1,20 +1,18 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ZhaMUtils.h"
+﻿#include "ZhaMUtils.h"
+#include "Modules/ModuleManager.h"
 
 #define LOCTEXT_NAMESPACE "FZhaMUtilsModule"
 
 void FZhaMUtilsModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	// 初始化逻辑（可为空）
 }
 
 void FZhaMUtilsModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+	// 清理逻辑（可为空）
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+
 IMPLEMENT_MODULE(FZhaMUtilsModule, ZhaMUtils)
